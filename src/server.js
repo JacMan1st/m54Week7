ce4d67ca5f400d64e9346b0a8342ab1830aebf67
Jack Mears <jack.mears@ymail.com>
2024-02-05 14:49:43 +0000

    enhancement: add two html with links

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ app.get("/health", (req, res) => {
   res.send("Healthy");
 });
 
+app.use("/example", express.static("example"));
+
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
