a66fc68134d0fce464ab7488c41448242cc41ea0
Jack Mears <jack.mears@ymail.com>
2024-02-06 13:43:54 +0000

    enhancement: add two routes

--- src/server.js
+++ src/server.js
@@ -2,13 +2,22 @@ const express = require("express");
 
 const app = express();
 
-// app.get("/health", (req, res) => {
-//   res.send("Healthy");
-// });
+// HTTP verbs - GET, POST, PUT, DELETE
 
-app.use("/example", express.static("example"));
+const fakeArr = [];
 
-app.use("/motorcycle", express.static("motorcycle"));
+app.use(express.json());
+
+// HTTP verb GET
+app.get("/getAllBooks", (request, responce) => {
+  responce.send({ message: "success", fakeArr: fakeArr });
+});
+
+app.post("/addBook", (request, response) => {
+  fakeArr.push(request.body);
+  console.log(fakeArr);
+  response.send({ message: "success", newBook: fakeArr[fakeArr.length - 1] });
+});
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
